3b0c0c5676a1dc7e757ae5448b79b2a0905bd5ab
Chris Ellis <chris@intrbiz.com>
2014-06-26 23:46:20 +0100

    Use new naming method

--- gerald-the-mole/src/main/java/com/intrbiz/gerald/sources/JVMSource.java
+++ gerald-the-mole/src/main/java/com/intrbiz/gerald/sources/JVMSource.java
@@ -3,57 +3,51 @@ package com.intrbiz.gerald.sources;
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.MetricRegistry;
 import com.intrbiz.gerald.source.AbstractIntelligenceSource;
-import com.intrbiz.gerald.witchcraft.Witchcraft;
 
 public class JVMSource extends AbstractIntelligenceSource
 {
     public JVMSource()
     {
-        super("Java Metrics");
+        super("com.intrbiz.JVMSource");
     }
     
     public void register(MetricRegistry registry)
     {
         this.registerJVMMetrics(registry);
         this.registerMemoryMetrics(registry);
-        this.registerThreadMetrics(registry);
-    }
-    
-    protected void registerThreadMetrics(MetricRegistry registry)
-    {
     }
     
     protected void registerJVMMetrics(MetricRegistry registry)
     {
-        registry.register(Witchcraft.name(JVMSource.class, "jvm-name"), new Gauge<String>(){
+        registry.register(name("jvm", "name"), new Gauge<String>(){
             @Override
             public String getValue()
             {
                 return System.getProperty("java.vendor");
             }
         });
-        registry.register(Witchcraft.name(JVMSource.class, "jvm-version"), new Gauge<String>(){
+        registry.register(name("jvm", "version"), new Gauge<String>(){
             @Override
             public String getValue()
             {
                 return System.getProperty("java.version");
             }
         });
-        registry.register(Witchcraft.name(JVMSource.class, "os-arch"), new Gauge<String>(){
+        registry.register(name("os", "arch"), new Gauge<String>(){
             @Override
             public String getValue()
             {
                 return System.getProperty("os.arch");
             }
         });
-        registry.register(Witchcraft.name(JVMSource.class, "os-name"), new Gauge<String>(){
+        registry.register(name("os", "name"), new Gauge<String>(){
             @Override
             public String getValue()
             {
                 return System.getProperty("os.name");
             }
         });
-        registry.register(Witchcraft.name(JVMSource.class, "os-version"), new Gauge<String>(){
+        registry.register(name("os", "version"), new Gauge<String>(){
             @Override
             public String getValue()
             {
@@ -64,21 +58,21 @@ public class JVMSource extends AbstractIntelligenceSource
     
     protected void registerMemoryMetrics(MetricRegistry registry)
     {
-        registry.register(Witchcraft.name(JVMSource.class, "memory-free"), new Gauge<Long>(){
+        registry.register(name("memory", "free"), new Gauge<Long>(){
             @Override
             public Long getValue()
             {
                 return (long) Runtime.getRuntime().freeMemory();
             }
         });
-        registry.register(Witchcraft.name(JVMSource.class, "memory-max"), new Gauge<Long>(){
+        registry.register(name("memory", "max"), new Gauge<Long>(){
             @Override
             public Long getValue()
             {
                 return (long) Runtime.getRuntime().maxMemory();
             }
         });
-        registry.register(Witchcraft.name(JVMSource.class, "memory-total"), new Gauge<Long>(){
+        registry.register(name("memory", "total"), new Gauge<Long>(){
             @Override
             public Long getValue()
             {
