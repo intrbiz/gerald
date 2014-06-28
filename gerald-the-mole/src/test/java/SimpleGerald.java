5f314f8449da18f04e51f2e6e26c40d4abf33c79
Chris Ellis <chris@intrbiz.com>
2014-06-28 16:36:56 +0100

    Move sample inline with changes

--- gerald-the-mole/src/test/java/SimpleGerald.java
+++ gerald-the-mole/src/test/java/SimpleGerald.java
@@ -1,13 +1,10 @@
 
-import java.util.concurrent.TimeUnit;
-
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import com.intrbiz.gerald.Gerald;
 import com.intrbiz.gerald.polyakov.Node;
-import com.intrbiz.gerald.sources.JVMSource;
 
 public class SimpleGerald
 {
@@ -17,11 +14,8 @@ public class SimpleGerald
         Logger.getRootLogger().setLevel(Level.TRACE);
         // setup Gerald to report
         Gerald.theMole()
-            .source(new JVMSource())
-            .lamplighter("some_key")
+            .lamplighter()
             .from(Node.service("SimpleGerald"))
-            .period(30, TimeUnit.SECONDS)
-            .courierTo("ws://127.0.0.1:8825/websocket")
             .start();
     }
 }
